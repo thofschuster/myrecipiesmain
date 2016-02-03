055b2d875c0d84beab5820ee0d8bcbabe825a3c3
Tim Hofschuster <tim@code-masters.de>
2016-02-03 11:57:36 +0100

    modded tests

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,11 +3,17 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 
   def setup
-    @recipe = Recipe.new(name: "Chicken", summary: "summary is here :D", description: "here comes description")
+    @chef = Chef.create(name: "Bob", email: "bob@example.com")
+    @recipe = @chef.recipes.build(name: "Chicken", summary: "summary is here :D", description: "here comes description")
   end
 
   test "recipe should be valid" do
     assert @recipe.valid?
+  end
+
+  test "chef_id should be present" do
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
   end
 
   test "name should be present" do
