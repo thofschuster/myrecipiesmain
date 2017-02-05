aaf1b66605337cad38c6e4ced86b090db889654a
thofschuster <assa.balder@googlemail.com>
2017-02-06 00:17:59 +0100

    adding flash messages. adding new form recipes. adding error display on invalid inputs in new form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,24 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+  def new
+    @recipe = Recipe.new
+  end
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(2)
+
+    if @recipe.save
+      flash[:success] = "Recipe has been saved here :)"
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
 end
