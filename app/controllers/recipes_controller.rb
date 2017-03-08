c36baae9c86e0237dc80c18932609455315cb441
thofschuster <assa.balder@googlemail.com>
2017-03-08 22:58:27 +0100

    adding edit function. fixing footer problem with long lists of steps.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -23,6 +23,21 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe has been updated here :)"
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+
   private
 
     def recipe_params
